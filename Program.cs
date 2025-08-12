ab786f2b57dcaad12d08e95582f08d11e67c32b0
ahmed elsaadany <ahmed.s.elsaadany@gmail.com>
2025-08-12 13:55:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace LinqAssingment01
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. Find all products that are out of stock.
+            //var Result = ListGenerator.ProductsList.Where(p => p.UnitsInStock == 0);
+            //foreach (var Unit in Result)
+            //{
+            //    Console.WriteLine(Unit);
+            //} 
+            #endregion
+            #region 2. Find all products that are in stock and cost more than 3.00 per unit.
+            //var Result = ListGenerator.ProductsList.Where(x => x.UnitsInStock > 0 && x.UnitPrice > 3);
+            //foreach (var Product in Result)
+            //{
+            //    Console.WriteLine(Product);
+            //} 
+            #endregion
+            #region 3. Returns digits whose name is shorter than their value.
+            String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            var Result = Arr.Where((name, value) => name.Length < value);
+            foreach (var name in Result)
+            {
+                Console.WriteLine(name);
+            }
+            #endregion
+        }
+    }
+}
