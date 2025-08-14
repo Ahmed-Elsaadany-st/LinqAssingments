28aa98ef6c3c998ea40de822b266e94bfbb64840
ahmed elsaadany <ahmed.s.elsaadany@gmail.com>
2025-08-14 22:15:26 +0300

    All done

--- Program.cs
+++ Program.cs
@@ -266,6 +266,20 @@ namespace LinqAssingment01
             //    Console.WriteLine(value);
             //}
             #endregion
+            #region 5. Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersB = { 1, 3, 5, 7, 8 };
+            var pairs = from a in numbersA
+                        from b  in numbersB
+                        where a < b
+                        select new { A = a, B = b }
+                      ;
+                Console.WriteLine("Pairs where a < b:");
+                 foreach (var pair in pairs)
+                 {
+                     Console.WriteLine($"{pair.A} is less than {pair.B}");
+                 }
+            #endregion
             #region 6. Select all orders where the order total is less than 500.00.
             //var OrderList = CustomersList.SelectMany(o => o.Orders).Where(o => o.Total < 500);
             //foreach (var order in OrderList)
